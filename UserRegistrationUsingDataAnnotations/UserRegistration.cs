c2c927a96bf489c5f9d48d02082cd2bb1e3042a8
ravina-24 <ravinamaudekar24@gmail.com>
2021-08-01 01:35:03 +0530

    [Ravina] Added : Declaring validations for  last name.

--- UserRegistrationUsingDataAnnotations/UserRegistration.cs
+++ UserRegistrationUsingDataAnnotations/UserRegistration.cs
@@ -7,17 +7,12 @@ namespace UserRegistrationUsingDataAnnotations
 {
     public class UserRegistration
     {
-        [Required(ErrorMessage = "First Name is required. It shouldn't be empty ")]
+        [Required(ErrorMessage = "Users {0} is required . ")]
         [StringLength(100, MinimumLength = 2, ErrorMessage = "Name should have minimum of 3 charaters and maximum 100 characters")]
-        [RegularExpression("^[A-Z][a-z]{2,}$", ErrorMessage = "Name is invalid ")]
         [DataType(DataType.Text)]
         public string FirstName1 { get; set; }
-
-
-
-        [Required(ErrorMessage = "Last Name is required. It shouldn't be empty ")]
+        
         [StringLength(100, MinimumLength = 3, ErrorMessage = "Last Name should have minimum of 3 charaters and maximum 100 characters")]
-        [RegularExpression("^[A-Z][a-z]{3,}$", ErrorMessage = "Last Name is invalid ")]
         [DataType(DataType.Text)]
         public string LastName1 { get; set; }
     }
