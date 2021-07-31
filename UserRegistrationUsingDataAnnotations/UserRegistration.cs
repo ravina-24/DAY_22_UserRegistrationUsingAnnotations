6602365e8bbf86b0f52111c0cc090ce68924e6cb
ravina-24 <ravinamaudekar24@gmail.com>
2021-08-01 01:39:31 +0530

    [Ravina] Added : Declaring validations for email address

--- UserRegistrationUsingDataAnnotations/UserRegistration.cs
+++ UserRegistrationUsingDataAnnotations/UserRegistration.cs
@@ -7,23 +7,16 @@ namespace UserRegistrationUsingDataAnnotations
 {
     public class UserRegistration
     {
-        [Required(ErrorMessage = "First Name is required. It shouldn't be empty ")]
+        [Required(ErrorMessage = "Users {0} is required. ")]
         [StringLength(100, MinimumLength = 2, ErrorMessage = "Name should have minimum of 3 charaters and maximum 100 characters")]
-        [RegularExpression("^[A-Z][a-z]{2,}$", ErrorMessage = "Name is invalid ")]
         [DataType(DataType.Text)]
         public string FirstName1 { get; set; }
-
-
-
-        [Required(ErrorMessage = "Last Name is required. It shouldn't be empty ")]
+        
         [StringLength(100, MinimumLength = 3, ErrorMessage = "Last Name should have minimum of 3 charaters and maximum 100 characters")]
-        [RegularExpression("^[A-Z][a-z]{3,}$", ErrorMessage = "Last Name is invalid ")]
         [DataType(DataType.Text)]
         public string LastName1 { get; set; }
-
-        [Required(ErrorMessage = "Email is required. It shouldn't be empty ")]
-        [RegularExpression("^[A-Z0-9a-z]{1,}([.#$^][A-Za-z0-9]+)?[@][A-Za-z]{2,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$", ErrorMessage = "Email Address is invalid ")]
-        [DataType(DataType.Text)]
+       
+        [DataType(DataType.EmailAddress)]
         public string EmailId1 { get; set; }
 
 
