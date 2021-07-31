0236510f62b2545b0853a3d7d72101a9e5e6a323
ravina-24 <ravinamaudekar24@gmail.com>
2021-08-01 01:17:23 +0530

    [Ravina] Added : Declaring Validations for the password.

--- UserRegistrationUsingDataAnnotations/UserRegistration.cs
+++ UserRegistrationUsingDataAnnotations/UserRegistration.cs
@@ -9,7 +9,6 @@ namespace UserRegistrationUsingDataAnnotations
     {
         [Required(ErrorMessage = "First Name is required. It shouldn't be empty ")]
         [StringLength(100, MinimumLength = 2, ErrorMessage = "Name should have minimum of 3 charaters and maximum 100 characters")]
-        [RegularExpression("^[A-Z][a-z]{2,}$", ErrorMessage = "Name is invalid ")]
         [DataType(DataType.Text)]
         public string FirstName1 { get; set; }
 
@@ -17,20 +16,23 @@ namespace UserRegistrationUsingDataAnnotations
 
         [Required(ErrorMessage = "Last Name is required. It shouldn't be empty ")]
         [StringLength(100, MinimumLength = 3, ErrorMessage = "Last Name should have minimum of 3 charaters and maximum 100 characters")]
-        [RegularExpression("^[A-Z][a-z]{3,}$", ErrorMessage = "Last Name is invalid ")]
         [DataType(DataType.Text)]
         public string LastName1 { get; set; }
 
         [Required(ErrorMessage = "Email is required. It shouldn't be empty ")]
-        [RegularExpression("^[A-Z0-9a-z]{1,}([.#$^][A-Za-z0-9]+)?[@][A-Za-z]{2,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$", ErrorMessage = "Email Address is invalid ")]
-        [DataType(DataType.Text)]
+        [DataType(DataType.EmailAddress)]
         public string EmailId1 { get; set; }
 
 
         [Required(ErrorMessage = "Phone Number is required. It shouldn't be empty ")]
-        
         [DataType(DataType.PhoneNumber)]
         public string PhoneNumber1 { get; set; }
+
+
+        [Required(ErrorMessage = "Password is required. It shouldn't be empty ")]
+        [StringLength(15, MinimumLength = 8, ErrorMessage = "Password should have minimum of 8 charaters ")]
+        [DataType(DataType.Text)]
+        public string Password1 { get; set; }
     }
 
 }
